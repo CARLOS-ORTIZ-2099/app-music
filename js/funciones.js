6b4a872322b5fde7b3ae2770474a52663184a299
Carlos Eduardo Ortiz <carlosklinton.2016@gmail.com>
2024-01-04 10:29:06 -0500

    se cambio de nombre la constante que hacia referencia al auido creado paso de container a audio

--- js/funciones.js
+++ js/funciones.js
@@ -1,11 +1,11 @@
 export default function values(arrayMusic, images) {
 
     const d = document;
-    const container = d.createElement("audio");
-    const imagesContainer = d.querySelector(".images-container");
+    const audio = d.createElement("audio");
     const image = d.createElement("img");
-    const rangoTiempo = document.querySelector("#rangoTiempo");
-    const aleatorio = document.querySelector(".aleatorio");
+    const imagesContainer = d.querySelector(".images-container");
+    const rangoTiempo = d.querySelector("#rangoTiempo");
+    const section = d.querySelector(".section");
     let sound =  d.querySelector('.sound')
     let mute =  d.querySelector('.mute')
     const rangoVolumen = document.getElementById("rangoVolumen");
@@ -14,27 +14,27 @@ export default function values(arrayMusic, images) {
    
     image.src = images[i];
     imagesContainer.appendChild(image);
-    container.src = arrayMusic[i];
+    audio.src = arrayMusic[i];
   
-    d.addEventListener("click", (e) => {
+    section.addEventListener("click", (e) => {
     
       const claseValor = e.target.classList;
       console.log(claseValor);
   
       switch (claseValor[0]) {
         case "play":
-          container.play();
+          audio.play();
           break;
 
         case "pause":
-          container.pause();
+          audio.pause();
           break;
 
         case "siguiente":
           i++;
           i > arrayMusic.length - 1 ? (i = 0) : "";
   
-          container.src = arrayMusic[i];
+          audio.src = arrayMusic[i];
           console.log(i);
           image.src = images[i];
           rangoTiempo.value = 0
@@ -44,7 +44,7 @@ export default function values(arrayMusic, images) {
           i--;
           i < 0 ? (i = arrayMusic.length - 1) : "";
   
-          container.src = arrayMusic[i];
+          audio.src = arrayMusic[i];
           console.log(i);
           image.src = images[i];
           rangoTiempo.value = 0
@@ -55,7 +55,7 @@ export default function values(arrayMusic, images) {
           i = Math.floor(Math.random() * arrayMusic.length);
           console.log(i);
   
-          container.src = arrayMusic[i];
+          audio.src = arrayMusic[i];
           image.src = images[i];
 
           rangoTiempo.value = 0
@@ -67,15 +67,15 @@ export default function values(arrayMusic, images) {
   
     rangoTiempo.addEventListener("input", (e) => {
         console.log(e.target.value)
-      container.currentTime = (rangoTiempo.value * container.duration) / 100;
+      audio.currentTime = (rangoTiempo.value * audio.duration) / 100;
     });
   
 
     rangoVolumen.addEventListener("input", (e) => {
 
-      container.volume = e.target.value;
-      console.log(container.volume)
-      if(container.volume == 0){
+      audio.volume = e.target.value;
+      console.log(audio.volume)
+      if(audio.volume == 0){
        
         sound.classList.add('hidden')
         mute.classList.remove('hidden')
